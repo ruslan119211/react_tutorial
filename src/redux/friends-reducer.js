3a5d460208abf6cc5e61fcfab2538390ec263d3f
Admin <ruslan119211@gmail.com>
2019-12-03 12:49:42 +0200

    refactor store.js

--- src/redux/friends-reducer.js
+++ src/redux/friends-reducer.js
@@ -4,7 +4,7 @@ let initialState = {
         {
             id: 1,
             name: 'Valua',
-            image: 'https://scontent.fnlv1-1.fna.fbcdn.net/v/t1.0-9/50982576_2017757371638622_1248444450603008000_n.jpg?_nc_cat=107&_nc_oc=AQndyW9QsR4jBVUBKgiAdLAMSIskmUN8mx9aZE3WeCiMp0rR7Qvn8DnZJ3VShd9K_eA&_nc_ht=scontent.fnlv1-1.fna&oh=4c6b015d0a499bbaf90b3db8a016124f&oe=5E284E5A'
+            image: 'https://media.thestar.com.my/Prod/BC31E7E4-06FF-40A1-8B75-E812DE06F54B'
         },
         {
             id: 2,
@@ -16,7 +16,7 @@ let initialState = {
         {
             id: 5,
             name: 'Valua',
-            image: 'https://scontent.fnlv1-1.fna.fbcdn.net/v/t1.0-9/50982576_2017757371638622_1248444450603008000_n.jpg?_nc_cat=107&_nc_oc=AQndyW9QsR4jBVUBKgiAdLAMSIskmUN8mx9aZE3WeCiMp0rR7Qvn8DnZJ3VShd9K_eA&_nc_ht=scontent.fnlv1-1.fna&oh=4c6b015d0a499bbaf90b3db8a016124f&oe=5E284E5A'
+            image: 'https://media.thestar.com.my/Prod/BC31E7E4-06FF-40A1-8B75-E812DE06F54B'
         },
         {
             id: 6,
