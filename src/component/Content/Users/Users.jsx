6234856326f06ff1132e85f428c8ef68c396fcac
Admin <ruslan119211@gmail.com>
2020-01-31 16:05:50 +0200

    refactor

--- src/component/Content/Users/Users.jsx
+++ src/component/Content/Users/Users.jsx
@@ -1,82 +1,33 @@
 import React from 'react';
+import * as axios from 'axios';
+import userPhotoDefault from '../../../assets/img/user-image.png';
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    followed: false,
-                    photoUrl: 'https://image.businessinsider.com/565dc87b84307663008b6652?width=1001&format=jpeg',
-                    fullName: 'Ivan',
-                    location: {city: 'Mykolaiv', country: 'Ukraine'},
-                    status: 'Iam free'
-                },
-                {
-                    id: 2,
-                    followed: true,
-                    photoUrl: 'https://image.businessinsider.com/565dc87b84307663008b6652?width=1001&format=jpeg',
-                    fullName: 'Kolya',
-                    location: {city: 'Mykolaiv', country: 'Ukraine'},
-                    status: 'Iam free1'
-                },
-                {
-                    id: 3,
-                    followed: true,
-                    photoUrl: 'https://image.businessinsider.com/565dc87b84307663008b6652?width=1001&format=jpeg',
-                    fullName: 'Vasya',
-                    location: {city: 'Mykolaiv', country: 'Ukraine'},
-                    status: 'Iam free'
-                },
-                {
-                    id: 4,
-                    followed: false,
-                    photoUrl: 'https://image.businessinsider.com/565dc87b84307663008b6652?width=1001&format=jpeg',
-                    fullName: 'Petro',
-                    location: {city: 'Mykolaiv', country: 'Ukraine'},
-                    status: 'Iam free'
-                },
-                {
-                    id: 5,
-                    followed: false,
-                    photoUrl: 'https://image.businessinsider.com/565dc87b84307663008b6652?width=1001&format=jpeg',
-                    fullName: 'Maksim',
-                    location: {city: 'Mykolaiv', country: 'Ukraine'},
-                    status: 'Iam free'
-                },
-                {
-                    id: 6,
-                    followed: true,
-                    photoUrl: 'https://image.businessinsider.com/565dc87b84307663008b6652?width=1001&format=jpeg',
-                    fullName: 'Mike',
-                    location: {city: 'Ukraine', country: 'Mykolaiv'},
-                    status: 'Iam free'
-                },
-                {
-                    id: 7,
-                    followed: false,
-                    photoUrl: 'https://image.businessinsider.com/565dc87b84307663008b6652?width=1001&format=jpeg',
-                    fullName: 'Roman',
-                    location: {city: 'Mykolaiv', country: 'Ukraine'},
-                    status: 'Iam free'
-                },
-                {
-                    id: 8,
-                    followed: false,
-                    photoUrl: 'https://image.businessinsider.com/565dc87b84307663008b6652?width=1001&format=jpeg',
-                    fullName: 'Max',
-                    location: {city: 'Mykolaiv', country: 'Ukraine'},
-                    status: 'Iam free too'
-                },
-            ]
-        )
+    let getUsers = () => {
+
+
+        if (props.users.length === 0) {
+
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                props.setUsers(response.data.items)
+            });
+    //             id: 1,
+            //             followed: false,
+            //             photoUrl: 'https://image.businessinsider.com/565dc87b84307663008b6652?width=1001&format=jpeg',
+            //             fullName: 'Ivan',
+            //             location: {city: 'Mykolaiv', country: 'Ukraine'},
+            //             status: 'Iam free'
+        }
     }
     return <div className='users-content'>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <div className='users-item'>
                     <div className='left-block'>
                         <div className='users-iamge'>
-                            <img src={u.photoUrl} alt={u.fullName}/>
+                            <img src={u.photos.small != null ? u.photos.small : userPhotoDefault} alt={u.name}/>
                         </div>
                         {u.followed
                             ? <button onClick={() => {
@@ -87,9 +38,9 @@ const Users = (props) => {
                             }}>Follow</button>}
                     </div>
                     <div className='right-block'>
-                        <span className='name-users'>{u.fullName}</span>
-                        <span className='status-users'>{u.status}</span>
-                        <span className='location-users'>{u.location.country},<br/>{u.location.city}</span>
+                        <p className='name-users'>{u.name}</p>
+                        <p className='status-users'>{u.status}</p>
+                        <p className='location-users'>{"u.location.country"},<br/>{"u.location.city"}</p>
                     </div>
                 </div>
             </div>)
